49fcbec699d9187a232c9e0a6dfc498b58b51294
EvanaSabatella1989 <eva.sabatella@gmail.com>
2024-10-18 18:58:17 -0300

    login

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs'); // Para hashear contraseñas
 const router = express.Router();
-const { leerUsuarios } = require('../data/db-usuarios'); // Simula un archivo JSON para usuarios
+//const { leerUsuarios } = require('../data/db-usuarios'); // Simula un archivo JSON para usuarios
 
 // Mostrar el formulario de login
 router.get('/login', (req, res) => {
@@ -9,23 +9,36 @@ router.get('/login', (req, res) => {
 });
 
 // Procesar inicio de sesión
-router.post('/login', async (req, res) => {
+// router.post('/login', async (req, res) => {
+//     const { email, password } = req.body;
+//     const usuarios = await leerUsuarios();
+
+//     // Buscar el usuario por email
+//     const usuario = usuarios.find(u => u.email === email);
+//     if (!usuario) {
+//         return res.status(400).send('Usuario no encontrado');
+//     }
+
+//     // Verificar la contraseña
+//     const passwordValida = await bcrypt.compare(password, usuario.password);
+//     if (!passwordValida) {
+//         return res.status(400).send('Contraseña incorrecta');
+//     }
+
+    
+//     res.redirect('/');
+// });
+
+router.post('/login',async (req, res) => {
     const { email, password } = req.body;
-    const usuarios = await leerUsuarios();
+    // Simular autenticación
+  if (email === 'admin@mail.com' && password === 'adminpass') { // Verificamos si son las credenciales del admin
+    res.redirect('/productos/admin'); // Redirigimos al panel de administración
+  } else if (email === 'user@mail.com' && password === 'userpass') { // Verificamos si son las credenciales del usuario
+    res.redirect('/productos'); // Redirigimos al panel de usuario
+  } else {
+    res.send('Credenciales incorrectas'); // Si las credenciales son incorrectas, enviamos un mensaje
+  }
+})
 
-    // Buscar el usuario por email
-    const usuario = usuarios.find(u => u.email === email);
-    if (!usuario) {
-        return res.status(400).send('Usuario no encontrado');
-    }
-
-    // Verificar la contraseña
-    const passwordValida = await bcrypt.compare(password, usuario.password);
-    if (!passwordValida) {
-        return res.status(400).send('Contraseña incorrecta');
-    }
-
-    // Aquí podrías guardar la sesión del usuario o generar un token JWT
-    res.redirect('/');
-});
 module.exports = router;
